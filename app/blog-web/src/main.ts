08fc7c02282b7a3c36adb8ff2ed258b34cd4ed88
suxin2017 <1107178482@qq.com>
2020-10-31 21:23:14 +0800

    init 项目

--- app/blog-web/src/main.ts
+++ app/blog-web/src/main.ts
@@ -1,7 +1,27 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 createApp(App)
   .use(router)
   .mount("#app");
+import ApolloClient, { gql } from "apollo-boost";
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: "http://localhost:4000/graphql"
+});
+
+apolloClient
+  .query({
+    query: gql`
+      query TestQuery {
+        books {
+          title
+          author
+          test
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+console.log(apolloClient);
